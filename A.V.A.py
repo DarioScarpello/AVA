c37fab6ae5daff376c004fe06ea52e05cd6963b8
Matija Jovanovic <jovanovic.m04@htlwienwest.at>
2022-04-05 12:06:02 +0200

    Last changes from April 5th
    Further development on the fetching of the altphrases for the list

--- A.V.A.py
+++ A.V.A.py
@@ -6,16 +6,20 @@ import psycopg2
 #tts
 engine = pyttsx3.init()
 
-#engine.say('Jutta Gefluegel')
-#engine.runAndWait()
+# engine.say('Hallo ich rede')
+# engine.runAndWait()
+
+# Creating the connection via the connection string to the database and the cursor to fetch the data 
+connection = psycopg2.connect("dbname=AVA user=postgres password=postgres") # Connectionstring verbergen !!!
+cur = connection.cursor()
+
+# the query that gets all the altphrases, which will be used throughout the whole program
+query = "select phrase from altphrases"    
+cur.execute(query)         
+# altphrases = cur.fetchall()                   # only .fetchall() returns a tuple, to use the "if term in list" method 
+altphrases = [r[0] for r in cur.fetchall()] 	# the tuple is converted into a normal list via the code on the left
 
 
-# connection = psycopg2.connect("dbname=AVA user=postgres password=postgres")
-# cur = connection.cursor()
-
-# query = "select phrase from altphrases"
-# cur.execute(query)
-# altphrases = cur.fetchall()
 
 
 
@@ -35,6 +39,7 @@ try:
 
 
     print(term)
+
 
     if "suche" in term.lower():
         searchterm = term.replace(" ", "+")
