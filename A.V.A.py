6451f3c2182ee0e6eb64d64485ec93cb502db83d
Matija Jovanovic <jovanovic.m04@htlwienwest.at>
2022-04-05 11:29:24 +0200

    Datenbanklogik Anfang
    Anfang der Datebanklogik

--- A.V.A.py
+++ A.V.A.py
@@ -9,17 +9,21 @@ engine = pyttsx3.init()
 #engine.say('Jutta Gefluegel')
 #engine.runAndWait()
 
-# # connection = psycopg2.connect("dbname=AVA user=postgres password=postgres")
-# # cur = connection.cursor()
-# # cur.execute("select * from keyphrases")
-# # output = cur.fetchall()
-# # print(output)
+
+connection = psycopg2.connect("dbname=AVA user=postgres password=postgres")
+cur = connection.cursor()
+
+query = "select phrase from altphrases"
+cur.execute(query)
+altphrases = cur.fetchall()
+
+
 
 #stt
 listener = sr.Recognizer()
 
 with sr.Microphone() as source:
-    listener.adjust_for_ambient_noise(source, duration=5)
+    listener.adjust_for_ambient_noise(source, duration=1)
     voice = listener.listen(source)
 
 print("Listenting...")
@@ -27,6 +31,9 @@ print("Listenting...")
 
 try:
     term = listener.recognize_google(voice, language="de-AT")
+    # if term in altphrases:
+
+
     print(term)
 
     if "suche" in term.lower():
