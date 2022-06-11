d9862473a23384f600a0ace69c85f30cb209d7a4
Jovanovic <74651542+JovanoMat@users.noreply.github.com>
2022-06-11 18:25:13 +0200

    abschlusssatz von ava
    Abschlussatz den AVA sagt, zum beenden der präsentation

--- A.V.A.py
+++ A.V.A.py
@@ -114,7 +114,11 @@ FloatLayout:
             saidterm = " " + listener.recognize_google(voice, language="de-AT") +  " "
             term = saidterm.lower()
             
-            
+            if "das war es mit unserer präsentation" in term:
+                speaker.say("vielen dank für ihre aufmerksamkeit, das AVA team und ich hoffen es hat ihnen gefallen. bei fragen stehen wir ihnen gerne zur verfügung.")
+                speaker.runAndWait()
+                return
+
             # go over every altphrase
             for phrase in altphrases:   
                 # check if an altphrase is present in the said term
